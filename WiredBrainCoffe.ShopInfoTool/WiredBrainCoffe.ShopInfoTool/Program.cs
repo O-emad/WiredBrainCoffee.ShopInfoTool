3e143a206b750419fb6a56078bf3d84b5b83129b
Omar Rokiba <orokiba@asu.edu>
2021-02-20 04:24:57 +0200

    Add Project

---
+++ WiredBrainCoffe.ShopInfoTool/WiredBrainCoffe.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffe.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+        }
+    }
+}
